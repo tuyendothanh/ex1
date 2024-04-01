f6177919137d7613ff755046aad8caf19f2cea25
ド タントゥェン <tuyendothanh@hakaru.jp>
2024-04-01 15:35:18 +0700

    package1

--- file1.go
+++
@@ -1,7 +0,0 @@
-package file1
-
-import "fmt"
-
-func FunctionInFile1() {
-	fmt.Println("Function in file1.go")
-}
---
+++ package1/package1.go
@@ -0,0 +1,7 @@
+package package1
+
+import "fmt"
+
+func FunctionInFile1() {
+	fmt.Println("Function in package1.go")
+}
